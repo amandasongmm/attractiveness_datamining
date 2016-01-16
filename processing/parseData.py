6b1b24ab1d1b41e6557bc5c9689c049a9709921a
Chad Atalla <catalla@ucsd.edu>
2016-01-16 12:57:11 -0800

    Successfully restructuring twin1's MIT data

--- processing/parseData.py
+++ processing/parseData.py
@@ -11,10 +11,33 @@
 #
 
 import pandas as pd 
+import numpy as np
 
-# Import data from csv into pandas dataframe
-data = pd.read_csv('../germine_CurrentBiology_twinaesthetics_PUBLIC.csv')
+## TODO
+## constants?
+## Pull second twin data...
+
+# Import first twins' MIT data from csv into pandas dataframe
+data = pd.read_csv('../germine_CurrentBiology_twinaesthetics_PUBLIC.csv', usecols=range(0,70))
+
+# Rename columns in dataframe to match our schema
+data.rename(columns={'Unnamed: 0': 'twin_pair_id', 'Twin_Num_of2.twin1': 'twin_id', 'sex_x.twin1' : 'gender', 'age.twin1' : 'age', 'Zygosity.twin1' : 'twin_type'}, inplace=True)
+
+# Melt the data to expand the MIT face columns onto multiple rows
+data = pd.melt(data, id_vars=['twin_pair_id', 'twin_id', 'gender', 'age', 'twin_type'], var_name='image_id', value_name='rating')
+
+# Fix numbering of images, MIT set will be 1 - 65
+data['image_id'] = data['image_id'].replace(['Faces.MIT', '.twin1'], ['',''], regex=True)
+data['image_id'] = data['image_id'].astype(int)
+
+# Re-order columns to match schema
+cols = ['image_id', 'twin_pair_id', 'twin_type', 'twin_id', 'age', 'gender', 'rating']
+data = data.ix[:, cols]
+
+# Sort dataframe
+data = data.sort_values(['image_id', 'twin_pair_id']);
 
 # Print out the dataframe's schema and columns
 print data.columns
+print '\n'
 print data
