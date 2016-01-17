5b47b9c7e85c661f6c59059c9c791ec9f66f84bb
Chad Atalla <catalla@ucsd.edu>
2016-01-17 03:04:46 -0800

    Loads twin1 and twin2 data into dataframe, proper schema

--- processing/parseData.py
+++ processing/parseData.py
@@ -13,10 +13,12 @@
 import pandas as pd 
 import numpy as np
 
-## TODO
-## constants?
-## Pull second twin data...
 
+
+'''
+	Label:		ParseTwin1Data
+	Purpose: 	Parse the data for twin 1 into a dataframe
+'''
 # Import first twins' MIT data from csv into pandas dataframe
 data = pd.read_csv('../data/germine_CurrentBiology_twinaesthetics_PUBLIC.csv', usecols=range(0,70))
 
@@ -34,10 +36,48 @@ data['image_id'] = data['image_id'].astype(int)
 cols = ['image_id', 'twin_pair_id', 'twin_type', 'twin_id', 'age', 'gender', 'rating']
 data = data.ix[:, cols]
 
-# Sort dataframe
-data = data.sort_values(['image_id', 'twin_pair_id']);
 
+
+'''
+	Label:		ParseTwin2Data
+	Purpose: 	Parse the data for twin 2 into a dataframe
+'''
+# Import second twins' MIT data from csv into pandas dataframe
+# Grab twin pair id from front, twin2 data from middle of csv
+data2 = pd.read_csv('../data/germine_CurrentBiology_twinaesthetics_PUBLIC.csv', usecols=(range(1) + range(395, 464)))
+
+# Rename columns in dataframe to match our schema
+data2.rename(columns={'Unnamed: 0': 'twin_pair_id', 'Twin_Num_of2.twin2': 'twin_id', 'sex_x.twin2' : 'gender', 'age.twin2' : 'age', 'Zygosity.twin2' : 'twin_type'}, inplace=True)
+
+# Melt the data to expand the MIT face columns onto multiple rows
+data2 = pd.melt(data2, id_vars=['twin_pair_id', 'twin_id', 'gender', 'age', 'twin_type'], var_name='image_id', value_name='rating')
+
+# Fix numbering of images, MIT set will be 1 - 65
+data2['image_id'] = data2['image_id'].replace(['Faces.MIT', '.twin2'], ['',''], regex=True)
+data2['image_id'] = data2['image_id'].astype(int)
+
+# Re-order columns to match schema
+data2 = data2.ix[:, cols]
+
+
+
+'''
+	Label:		SortValues
+	Purpose:	Combine and sort the data from the two twins
+'''
+# Combine twin1 and twin2 data
+allData = data.append(data2)
+
+# Sort dataframe
+allData = allData.sort_values(['image_id', 'twin_pair_id', 'twin_id']);
+
+
+
+'''
+	Label:		PrintResults
+	Purpose:	Print out the dataframe and schema to verify correctness
+'''
 # Print out the dataframe's schema and columns
-print data.columns
+print allData.columns
 print '\n'
-print data
+print allData
