1eef5c647d565faccbfc27ae2b33c477acbf348b
Chad Atalla <catalla@ucsd.edu>
2016-01-24 01:30:37 -0800

    Modularize parser, parse all datasets

--- processing/parseData.py
+++ processing/parseData.py
@@ -2,74 +2,38 @@
 # parseData.py
 #
 # The purpose of this script is to parse the twin attractiveness data
-# into a pandas dataframe, sort, preprocess, and save to a csv.
+# of all image sets into a dataframe, sort, preprocess, and save to a csv.
 #
 # Built for Python2 in order to work best with machine learning libraries.
 #
-# Authors: 	Amanda Song, Chad Atalla
-# Date: 	1/15/2016
+# Author: 	Chad Atalla
+# Date: 	1/23/2016
 #
 
+
+
+from parseSingleSet import parseSet
 import pandas as pd 
 import numpy as np
+import re
 
 
 
 '''
-	Label:		ParseTwin1Data
-	Purpose: 	Parse the data for twin 1 into a dataframe
+	Label:		ParseAllSets
+	Purpose:	Parse the 4 different image sets
 '''
-# Import first twins' MIT data from csv into pandas dataframe
-data = pd.read_csv('../data/germine_CurrentBiology_twinaesthetics_PUBLIC.csv', usecols=range(0,70))
+# Beginning indexes of MIT Dataset
+allData = parseSet(5,399)
 
-# Rename columns in dataframe to match our schema
-data.rename(columns={'Unnamed: 0': 'twin_pair_id', 'Twin_Num_of2.twin1': 'twin_id', 'sex_x.twin1' : 'gender', 'age.twin1' : 'age', 'Zygosity.twin1' : 'twin_type'}, inplace=True)
+# Beginning indexes of Glascow Dataset
+allData = allData.append(parseSet(71, 465))
 
-# Melt the data to expand the MIT face columns onto multiple rows
-data = pd.melt(data, id_vars=['twin_pair_id', 'twin_id', 'gender', 'age', 'twin_type'], var_name='image_id', value_name='rating')
+# Beginning indexes of Non-Glascow Dataset
+allData = allData.append(parseSet(201, 595))
 
-# Fix numbering of images, MIT set will be 1 - 65
-data['image_id'] = data['image_id'].replace(['Faces.MIT', '.twin1'], ['',''], regex=True)
-data['image_id'] = data['image_id'].astype(int)
-
-# Re-order columns to match schema
-cols = ['image_id', 'twin_pair_id', 'twin_type', 'twin_id', 'age', 'gender', 'rating']
-data = data.ix[:, cols]
-
-
-
-'''
-	Label:		ParseTwin2Data
-	Purpose: 	Parse the data for twin 2 into a dataframe
-'''
-# Import second twins' MIT data from csv into pandas dataframe
-# Grab twin pair id from front, twin2 data from middle of csv
-data2 = pd.read_csv('../data/germine_CurrentBiology_twinaesthetics_PUBLIC.csv', usecols=(range(1) + range(395, 464)))
-
-# Rename columns in dataframe to match our schema
-data2.rename(columns={'Unnamed: 0': 'twin_pair_id', 'Twin_Num_of2.twin2': 'twin_id', 'sex_x.twin2' : 'gender', 'age.twin2' : 'age', 'Zygosity.twin2' : 'twin_type'}, inplace=True)
-
-# Melt the data to expand the MIT face columns onto multiple rows
-data2 = pd.melt(data2, id_vars=['twin_pair_id', 'twin_id', 'gender', 'age', 'twin_type'], var_name='image_id', value_name='rating')
-
-# Fix numbering of images, MIT set will be 1 - 65
-data2['image_id'] = data2['image_id'].replace(['Faces.MIT', '.twin2'], ['',''], regex=True)
-data2['image_id'] = data2['image_id'].astype(int)
-
-# Re-order columns to match schema
-data2 = data2.ix[:, cols]
-
-
-
-'''
-	Label:		SortValues
-	Purpose:	Combine and sort the data from the two twins
-'''
-# Combine twin1 and twin2 data
-allData = data.append(data2)
-
-# Sort dataframe
-allData = allData.sort_values(['image_id', 'twin_pair_id', 'twin_id']);
+# Beginning indexes of 
+allData = allData.append(parseSet(266, 660))
 
 
 
---
+++ processing/parseSingleSet.py
@@ -0,0 +1,73 @@
+#
+# parseSingleSet.py
+#
+# The purpose of this function is to parse the ratings from both twins for a
+# single set of images (MIT, Glascow, etc.)
+#
+# Author: 	Chad Atalla
+# Date: 	1/23/2016
+#
+
+
+
+import pandas as pd 
+import numpy as np
+import re
+
+
+
+def parseSet (twin1Start, twin2Start):
+	'''
+		Label:		ParseTwinData
+		Purpose: 	Parse the data for twin 1 and 2 into a dataframe
+	'''
+	# Import first twins' MIT data from csv into pandas dataframe
+	data = pd.read_csv('../data/germine_CurrentBiology_twinaesthetics_PUBLIC.csv', usecols=(range(5) + range(twin1Start, twin1Start+50)))
+
+	# Rename columns in dataframe to match our schema
+	data.rename(columns={'Unnamed: 0': 'twin_pair_id', 'Twin_Num_of2.twin1': 'twin_id', 'sex_x.twin1' : 'gender', 'age.twin1' : 'age', 'Zygosity.twin1' : 'twin_type'}, inplace=True)
+
+	# Import second twins' MIT data from csv into pandas dataframe
+	# Grab twin pair id from front, twin2 data from middle of csv
+	data2 = pd.read_csv('../data/germine_CurrentBiology_twinaesthetics_PUBLIC.csv', usecols=(range(1) + range(395, 399) + range(twin2Start, twin2Start+50)))
+
+	# Rename columns in dataframe to match our schema
+	data2.rename(columns={'Unnamed: 0': 'twin_pair_id', 'Twin_Num_of2.twin2': 'twin_id', 'sex_x.twin2' : 'gender', 'age.twin2' : 'age', 'Zygosity.twin2' : 'twin_type'}, inplace=True)
+
+
+
+	'''
+		Label:		CombineData
+		Purpose: 	Melt and combine the data for twin 1 and 2
+	'''
+	# Melt the data to expand the MIT face columns onto multiple rows
+	data = pd.melt(data, id_vars=['twin_pair_id', 'twin_id', 'gender', 'age', 'twin_type'], var_name='image_id', value_name='rating')
+
+	# Melt the data to expand the MIT face columns onto multiple rows
+	data2 = pd.melt(data2, id_vars=['twin_pair_id', 'twin_id', 'gender', 'age', 'twin_type'], var_name='image_id', value_name='rating')
+
+	# Append data
+	data = data.append(data2)
+
+
+
+	'''
+		Label:		ProcessData
+		Purpose:	Map the data to our desired schema
+	'''
+
+	# Create column for indicating picture dataset (Strip face and twin info)
+	data['dataset'] = data['image_id'].map(lambda x: x[6: (re.search('\d', x)).start()])
+
+	# Fix numbering of images
+	data['image_id'] = data['image_id'].map(lambda x: x[re.search('\d', x).start() : x.find('.', 7)])
+	data['image_id'] = data['image_id'].astype(int)
+
+	# Re-order columns to match schema
+	cols = ['dataset', 'image_id', 'twin_pair_id', 'twin_type', 'twin_id', 'age', 'gender', 'rating']
+	data = data.ix[:, cols]
+
+	# Sort dataframe
+	data = data.sort_values(['dataset', 'image_id', 'twin_pair_id', 'twin_id'])
+
+	return data
