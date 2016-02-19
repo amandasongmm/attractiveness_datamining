9050d2908ea77e48dcee9e4877a1fcd0e42890cc
Chad Atalla <catalla@ucsd.edu>
2016-02-19 00:17:53 -0800

    Ran PCA on the twin set, saved 6 PCs

---
+++ processing/TwinSetPCA.py
@@ -0,0 +1,50 @@
+"""
+TwinSetPCA.py
+
+The purpose of this script is to run PCA on the features from the twin set
+
+Built for Python2 in order to work best with machine learning libraries.
+
+Author: Chad Atalla
+Date:   2/17/2016
+"""
+
+
+
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+
+
+
+#
+# Label:   LoadData
+# Purpose: Load in the twin set feature matrix
+#
+data = pd.read_csv('./landmarking/allFeatures.csv', usecols=range(2,31))
+npData = data.as_matrix()
+
+
+
+#
+# Label:   RunPCA
+# Purpose: Run PCA on the features to determine dimensionality
+#
+pca = PCA(n_components=.99)
+newFeatures = pca.fit_transform(npData)
+
+# Put the new feature matrix into a dataframe
+newData = pd.DataFrame(newFeatures)
+
+
+
+#
+# Label:   PrintAndSave
+# Purpose: Print and save PCA results and new features
+#
+print '\nvariance ratios = '
+print pca.explained_variance_ratio_
+print '\nn_components = '
+print pca.n_components_
+
+newData.to_csv('PCA_Features.csv')
