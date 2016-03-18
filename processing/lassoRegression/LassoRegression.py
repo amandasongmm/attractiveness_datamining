1b41934f7f3d8740f98025b737ef4adb21084456
Chad Atalla <catalla@ucsd.edu>
2016-03-18 00:50:16 -0700

    Added correct cross validation to Lasso and Linear regressors

--- processing/lassoRegression/LassoRegression.py
+++ processing/lassoRegression/LassoRegression.py
@@ -22,7 +22,8 @@ from sklearn.cross_validation import train_test_split
 
 
 NUM_FEATS = 29
-alpha = .000002
+TRIALS = 5          # Num of test / train splits to test on
+alpha = .00002
 
 
 # Load in the original features
@@ -32,34 +33,51 @@ x = preprocessing.scale(x)
 # Load in everyone's ratings for the 200 faces
 yData = pd.read_csv('ratingMatrixChad.csv')
 
-# Split ratings into test and train set
-yTrain, yTest = train_test_split(yData, train_size=.8)
-
 # Dataframe to hold the scores of each linear model
-varScore = pd.Series(yTrain.index.size)
+varScore = pd.Series(yData.index.size)
+correlation = pd.Series(yData.index.size)
+MSEs = pd.Series(yData.index.size)
 
-# Iterate over each person's 200 ratings
+# Iterate over each person's ratings
 i = 0
-for row, index in yTrain.iterrows():
+for row, index in yData.iterrows():
     # Take one person's ratings at a time
     y = index.as_matrix()
 
-    # Run Linear regression
-    myModel = linear_model.Lasso(alpha=alpha, fit_intercept=True, copy_X=True, normalize=True)
-    myModel.fit(x,y)
+    # Hold aggregate model data for different test / train splits
+    scores = pd.Series(TRIALS)
+    mses = pd.Series(TRIALS)
+    predictions = pd.Series()
+    yVals = pd.Series()
 
-    # Save the calculated variance scores
-    k = 0
-    total = 0
-    for j, vals in yTest.iterrows():
-        total += myModel.score(x,vals)
-        k += 1
-    total /= k
+    # Do multiple train / test splits
+    for j in range(TRIALS):
+        # Split ratings into test and train set
+        yTrain, yTest, xTrain, xTest = train_test_split(y, x, train_size=.99)
 
-    print myModel.score(x,y)
-    varScore.loc[i] = total
-    print total
+        # Run Lasso regression
+        myModel = linear_model.Lasso(alpha=alpha, fit_intercept=True, copy_X=True, normalize=True)
+        myModel.fit(xTrain,yTrain)
+
+        scores.loc[j] = myModel.score(xTest, yTest)
+
+        # Save the predicted ratings
+        prediction = myModel.predict(xTest)
+        predictions = predictions.append(pd.Series(prediction))
+        yVals = yVals.append(pd.Series(yTest))
+        mses.loc[j] = np.mean((prediction - yTest) ** 2)
+
+    # Save the calculated variance score
+    varScore.loc[i] = np.mean(scores)
+
+    # Save the MSE vals
+    MSEs.loc[i] = np.mean(mses)
+
+    # Save the correlation
+    correlation.loc[i] = (np.corrcoef(predictions, yVals)[0][1])
 
     i += 1
 
+MSEs.to_csv(('mse' + str(alpha) + '.csv'), index=False)
 varScore.to_csv(('varianceScore' + str(alpha) + '.csv'), index=False)
+correlation.to_csv(('correlation' + str(alpha) + '.csv'), index=False)
--- processing/linearRegression/crossValidated/LinearRegression.py
+++ processing/linearRegression/crossValidated/LinearRegression.py
@@ -7,7 +7,7 @@ the weights of the different primary components from PCA on the twin set
 Built for Python2 in order to work best with machine learning libraries.
 
 Author: Chad Atalla
-Date:   2/18/2016
+Date:   3/17/2016
 """
 
 
@@ -16,14 +16,17 @@ import pandas as pd
 import numpy as np
 import math
 from sklearn import linear_model
+from sklearn import preprocessing
+from sklearn.cross_validation import train_test_split
 
 
-NUM_PCS = 10
+NUM_PCS = 11
+TRIALS = 5
 
 
 # Load in the 6 PCs for image features
 x = pd.read_csv('PCA_Features.csv')
-x = x.as_matrix()
+x = preprocessing.scale(x)
 
 # Load in everyone's ratings for the 200 faces
 yData = pd.read_csv('ratingMatrixChad.csv')
@@ -41,32 +44,40 @@ for row, index in yData.iterrows():
     # Take one person's ratings at a time
     y = index.as_matrix()
 
-    # Run Linear regression
-    myModel = linear_model.LinearRegression(fit_intercept=True, copy_X=True, normalize=True)
-    myModel.fit(x,y)
+    # Hold aggregate model data for different test / train splits
+    scores = pd.Series(TRIALS)
+    mses = pd.Series(TRIALS)
+    predictions = pd.Series()
+    yVals = pd.Series()
 
-    # Save the calculated coefficients
-    coefficients.loc[i] = myModel.coef_
+    # Do multiple train / test splits
+    for j in range(TRIALS):
+        # Split ratings into test and train set
+        yTrain, yTest, xTrain, xTest = train_test_split(y, x, train_size=.99)
 
-    # Save the calculated variance scores
-    varScore.loc[i] = myModel.score(x,y)
+        # Run Linear regression
+        myModel = linear_model.LinearRegression(fit_intercept=True, copy_X=True, normalize=True)
+        myModel.fit(x,y)
 
-    # Save the calculated MSE
-    prediction = myModel.predict(x)
-    MSEs.loc[i] = np.mean((prediction - y) ** 2)
+        scores.loc[j] = myModel.score(xTest, yTest)
 
-    # Save the predicted ratings
-    predicted.loc[i] = prediction
+        # Save the predicted ratings
+        prediction = myModel.predict(xTest)
+        predictions = predictions.append(pd.Series(prediction))
+        yVals = yVals.append(pd.Series(yTest))
+        mses.loc[j] = np.mean((prediction - yTest) ** 2)
+
+    # Save the calculated variance score
+    varScore.loc[i] = np.mean(scores)
+
+    # Save the MSE vals
+    MSEs.loc[i] = np.mean(mses)
 
     # Save the correlation
-    correlation.loc[i] = (np.corrcoef(prediction, y))[0][1]
-    if math.isnan(correlation.loc[i]):
-        correlation.loc[i] = 0
+    correlation.loc[i] = (np.corrcoef(predictions, yVals)[0][1])
 
-    i = i + 1
+    i += 1
 
-coefficients.to_csv('linearCoefficients.csv', index=False)
 varScore.to_csv('varianceScore.csv', index=False)
 MSEs.to_csv('mseVals.csv', index=False)
-predicted.to_csv('predictedRatings.csv', index=False)
 correlation.to_csv('correlations.csv', index=False)
