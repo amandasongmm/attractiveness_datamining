77de5c9c6e545c0bb585f3753e50eae2c59c8d5e
linjieli <linjieli222@gmail.com>
2016-05-27 12:46:38 -0700

    update nn

--- linjieCode/code/xVal_train_test.py
+++ linjieCode/code/xVal_train_test.py
@@ -9,7 +9,7 @@ import random
 import math
 
 prediction_model = linear_model.Ridge(fit_intercept=True)
-def crossVal(mean_rating, featureMat, pModel = prediction_model, valSize= 0.1, MODEL= 'config'):
+def crossVal(mean_rating, featureMat, pModel = prediction_model, valSize= 0.1, MODEL= 'config',printResult = False):
     # using cross validation to find the optimal number of features
     X_train, X_test, y_train, y_test = cross_validation.train_test_split( \
         featureMat, mean_rating, random_state=0, test_size=valSize)
@@ -17,9 +17,9 @@ def crossVal(mean_rating, featureMat, pModel = prediction_model, valSize= 0.1, M
     varList = []
     mseList = []
     if MODEL != 'faceSNN':
-        numFeature = [40, 50, 60, 65, 70, 75, 80, 90, 100, 120, 150, 200, 250, 300, 350]
+        numFeature = np.linspace(20,300,num = 100,dtype = np.int16)
     else:
-        numFeature = [10, 20, 30, 40, 50]
+        numFeature = np.linspace(0,50,num = 10,dtype = np.int16 )
     for numF in numFeature:
         X_train_hat = X_train[:, :numF]
         X_test_hat = X_test[:, :numF]
@@ -43,64 +43,86 @@ def crossVal(mean_rating, featureMat, pModel = prediction_model, valSize= 0.1, M
         # Calculate the correlation between prediction and actual rating.
         cor = np.corrcoef(predicted_rating, y_test)
         corrList.append(cor[0, 1])
-
-    print 'Correlation: ', max(corrList)
-    print 'num of features: ', numFeature[np.argmax(corrList)]
-    print 'R^2 score: ', max(varList)
-    print 'num of features: ', numFeature[np.argmax(varList)]
-    print 'MSE: ', min(mseList)
-    print 'num of features: ', numFeature[np.argmin(mseList)]
+    if printResult:
+        print 'Correlation: ', max(corrList)
+        print 'num of features: ', numFeature[np.argmax(corrList)]
+        print 'R^2 score: ', max(varList)
+        print 'num of features: ', numFeature[np.argmax(varList)]
+        print 'MSE: ', min(mseList)
+        print 'num of features: ', numFeature[np.argmin(mseList)]
     optNumFea = numFeature[np.argmax(corrList)]
     X_train_hat = X_train[:, :optNumFea]
     return X_train_hat, optNumFea
 
 
-def Train_Test(mean_rating, featureMat, pModel = prediction_model, hyperParam = None,\
-               numTrain = 20,savePath = '../Result',MODEL= 'config', printToFile = False):
+def Train_Test(mean_rating, featureMat, pModel = prediction_model, hyperParam = None, xVal = False,\
+               numTrain = 20,savePath = '../Result',MODEL= 'config', printToFile = False,ratio = 0.5):
     if hyperParam != None :
         featureMat_hat = featureMat[:, :hyperParam]
     else:
         featureMat_hat = featureMat
     dataLen = featureMat_hat.shape[0]
-
-    corrList = []
-    varList = []
-    mseList = []
+    corrValiList = []
+    varValiList = []
+    mseValiList = []
+    corrTrainList = []
+    varTrainList = []
+    mseTrainList = []
     index_list = range(dataLen)
     for i in range(1, numTrain):
-        index_random = random.shuffle(index_list)
-        train_index = index_random[:math.ceil(float(dataLen)/2)]
-        test_index = index_random[math.ceil(float(dataLen)/2):]
+        index_random  = index_list
+        random.shuffle(index_random)
+        train_index = index_random[:int(math.ceil(float(dataLen)*ratio))]
+        test_index = index_random[int(math.ceil(float(dataLen)*ratio)):]
         feature_train, feature_test = featureMat_hat[train_index], featureMat_hat[test_index]
         rating_train, rating_test = mean_rating[train_index], mean_rating[test_index]
-        if hyperParam != None :
-            feature_train, optNumFea = crossVal(rating_train,feature_train,pModel = pModel, MODEl = MODEL)
+        if hyperParam == None and xVal:
+            _, optNumFea = crossVal(rating_train,feature_train,pModel = pModel, MODEL = MODEL)
+            feature_train = feature_train[:,:optNumFea]
             feature_test = feature_test[:,:optNumFea]
         # Do linear regression on feature_arr and mean_rating
         pModel.fit(feature_train, rating_train)
+        
+        ######### on validation set #############
         predicted_rating = pModel.predict(feature_test)
-
         # Calculate the mean square error
         MSE = np.mean((predicted_rating - rating_test) ** 2)
-        mseList.append(MSE)
-
+        mseValiList.append(MSE)
         # Returns the coefficient of determination R^2 of the prediction.
         variance_score = pModel.score(feature_test, rating_test)
-        varList.append(variance_score)
-
+        varValiList.append(variance_score)
         # Calculate the correlation between prediction and actual rating.
         cor = np.corrcoef(predicted_rating, rating_test)
-        corrList.append(cor[0, 1])
-
-    print 'Residual sum of squares: %.2f' % (sum(mseList) / numTrain)
-    print 'Variance score is: %.2f' % (sum(varList) / numTrain)
-    print 'Correlation between predicted ratings and actual ratings is: %.4f' % (sum(corrList) / numTrain)
+        corrValiList.append(cor[0, 1])
+        
+        ############# on train set ############
+        predicted_rating_train = pModel.predict(feature_train)
+        # Calculate the mean square error
+        MSE = np.mean((predicted_rating_train - rating_train) ** 2)
+        mseTrainList.append(MSE)
+        # Returns the coefficient of determination R^2 of the prediction.
+        variance_score = pModel.score(feature_train, rating_train)
+        varTrainList.append(variance_score)
+        # Calculate the correlation between prediction and actual rating.
+        cor = np.corrcoef(predicted_rating_train, rating_train)
+        corrTrainList.append(cor[0, 1])
+        
+    print 'number of features: %d' % optNumFea
+    print 'On validation set:'
+    print 'Residual sum of squares: %.2f' % (sum(mseValiList) / numTrain)
+    print 'Variance score is: %.2f' % (sum(varValiList) / numTrain)
+    print 'Correlation between predicted ratings and actual ratings is: %.4f' % (sum(corrValiList) / numTrain)
+    print ' '
+    print 'On training set:'
+    print 'Residual sum of squares: %.2f' % (sum(mseTrainList) / numTrain)
+    print 'Variance score is: %.2f' % (sum(varTrainList) / numTrain)
+    print 'Correlation between predicted ratings and actual ratings is: %.4f' % (sum(corrTrainList) / numTrain)
 
     if printToFile:
         fName = savePath + '/' + MODEL + '_kFold.txt'
         with open(fName, 'w') as f:
             f.write('Number of train/test: %d' % numTrain + '\n')
-            f.write('Residual sum of squares: %.2f' % (sum(mseList) / numTrain) + '\n')
-            f.write('Variance score is: %.2f' % (sum(varList) / numTrain) + '\n')
+            f.write('Residual sum of squares: %.2f' % (sum(mseValiList) / numTrain) + '\n')
+            f.write('Variance score is: %.2f' % (sum(varValiList) / numTrain) + '\n')
             f.write('Correlation between predicted ratings and actual ratings is: %.4f' \
-                    % (sum(corrList) / numTrain) + '\n')
+                    % (sum(corrValiList) / numTrain) + '\n')
