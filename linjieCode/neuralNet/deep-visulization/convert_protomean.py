e4bc33f25997aa4e9d9116dd62de2dcb736a5e55
linjieli222 <linjieli222@gmail.com>
2016-09-09 12:20:05 -0400

    added convert protomean python file

--- linjieCode/neuralNet/deep-visulization/convert_protomean.py
+++ linjieCode/neuralNet/deep-visulization/convert_protomean.py
@@ -3,8 +3,8 @@ import numpy as np
 import sys
 
 if len(sys.argv) != 3:
-print "Usage: python convert_protomean.py proto.mean out.npy"
-sys.exit()
+    print "Usage: python convert_protomean.py proto.mean out.npy"
+    sys.exit()
 
 blob = caffe.proto.caffe_pb2.BlobProto()
 data = open( sys.argv[1] , 'rb' ).read()
